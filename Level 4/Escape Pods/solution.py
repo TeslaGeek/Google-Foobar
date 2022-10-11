704e200d7d6271b6ae40c595de8c3ce0b1bd3eb1
Parissa Chilar <parisa.eslambolchilar@gmail.com>
2022-10-11 11:20:17 +0100

    Update solution.py

--- Level 4/Escape Pods/solution.py
+++ Level 4/Escape Pods/solution.py
@@ -1,32 +1,36 @@
 # This challenge is related to data structures and in partciular graph data structures.
 # if unfamilar with graph data structures this website is a great resource: https://www.geeksforgeeks.org/graph-data-structure-and-algorithms/?ref=lbp
-# To solve this challenge I had to remind myself about different search methods in graphs: breadth first search (BFS) vs depth first search (DFS). 
+# To solve this challenge I had to remind myself about different search methods in graphs: breadth first search (BFS) vs depth first search (DFS).
 # Then I started reading on max flow problem https://www.geeksforgeeks.org/max-flow-problem-introduction/ and various methods proposed to solve the
 # max flow problem. It is an interesting problem space with great solutions proposed by Ford-Fulkerson (https://www.geeksforgeeks.org/ford-fulkerson-algorithm-for-maximum-flow-problem/)
-# and Dinic (https://www.geeksforgeeks.org/dinics-algorithm-maximum-flow/). Alhtough the outcomes of both algorithms are identical, Dinic's is more time 
+# and Dinic (https://www.geeksforgeeks.org/dinics-algorithm-maximum-flow/). Alhtough the outcomes of both algorithms are identical, Dinic's is more time
 # efficient and knowing how Foobar codes are assessed (low execution time is preferred), I opted for Dinic's algorithm.
-# 
+#
 # The essence of Dinic's algorithm is:
 
 # * A flow is maximum if there is no s to t path in residual graph.
-# * BFS is used in a loop. There is a difference though in the way we use BFS in both algorithms (Ford-Fulkerson and Dinic).
+# * BFS is used in a loop. There is a difference though in the way we use BFS in both algorithms
+# (Ford-Fulkerson and Dinic).
 #
-# In Ford-Fulkerson's algorithm, we use BFS to find an augmenting path and send flow across this path. In Dinic’s algorithm, we use BFS to check if more 
-# flow is possible and to construct level graph. 
-# In level graph, we assign levels to all nodes, level of a node is shortest distance (in terms of number of edges) of the node from source. 
-# Once level graph is constructed, we send multiple flows using this level graph. 
-# This is the reason it works better than Ford-Fulkerson. In Ford-Fulkerson, we send only flow that is send across the path found by BFS.
+# In Ford-Fulkerson`s algorithm, we use BFS to find an augmenting path and send flow across this path. In Dinic`s
+# algorithm, we use BFS to check if more flow is possible and to construct level graph.
+# In level graph, we assign levels to all nodes, level of a node is shortest distance (in terms of number of edges) of
+# the node from source. Once level graph is constructed, we send multiple flows using this level graph.
+# This is the reason it works better than Ford-Fulkerson. In Ford-Fulkerson, we send only flow that is send across the
+# path found by BFS.
 
-# Outline of Dinic’s algorithm : 
+# Outline of Dinic`s algorithm :
 #
 # Initialize residual graph G as given graph.
 # Do BFS of G to construct a level graph (or assign levels to vertices) and also check if more flow is possible.
 # If more flow is not possible, then return
-# Send multiple flows in G using level graph until blocking flow is reached. 
+# Send multiple flows in G using level graph until blocking flow is reached.
 # Here using level graph means, in every flow, levels of path nodes should be 0, 1, 2 ... (in order) from s to t.
-# I re-used the BFS algorithm in https://www.geeksforgeeks.org/dinics-algorithm-maximum-flow/ and worked on the rest of the solution as below.
-# The code below passed all the tests on the Foobar. This is a non trivial challenge for sure and almost took 12 days (out of assigned 14 days) 
-# to complete. 
+# I re-used the BFS algorithm in https://www.geeksforgeeks.org/dinics-algorithm-maximum-flow/ and worked on the rest of
+# the solution as below.
+#
+# The code below passed all the tests on the Foobar. This is a non trivial challenge for sure and almost took 12 days
+# (out of assigned 14 days) to complete.
 
 
 def bfs(matrix, source, destination):
@@ -103,3 +107,6 @@ def solution(entrances, exits, path):
     for i in range(len(aug)):
         total += aug[-2][i][0]
     return total
+
+print(solution([0], [3], [[0, 7, 0, 0], [0, 0, 6, 0], [0, 0, 0, 8], [9, 0, 0, 0]]))
+print(solution([0, 1], [4, 5], [[0, 0, 4, 6, 0, 0], [0, 0, 5, 2, 0, 0], [0, 0, 0, 0, 4, 4], [0, 0, 0, 0, 6, 6], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
